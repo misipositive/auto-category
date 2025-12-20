526a86407a01e3bddd91e8cf56466976e8d422d1
misipositive <117873611+misipositive@users.noreply.github.com>
2025-12-20 14:04:45 +0100

    fix typo

--- auto-category4.py
+++ auto-category4.py
@@ -199,7 +199,7 @@ def start_oauth_flow():
             test_socket.bind(('localhost', port))
             test_socket.close()
         except OSError:
-            script_log(f"ERROR: Port {port} is already in use. Please close the application using it or change the port in the script (line 202) and Twitch Developer Console.")
+            script_log(f"ERROR: Port {port} is already in use. Please close the application using it or change the port in the script (line 194) and Twitch Developer Console.")
             return
         
         redirect_uri = f"http://localhost:{port}"
